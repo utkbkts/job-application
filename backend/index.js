9b99629d253437fb8da75399eeeb6c82aa859b28
utku <utkbktss5@gmail.com>
2024-10-27 00:08:53 +0300

    helmet updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import cors from "cors";
 import ConnectedMongoDB from "./db/connected.mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
@@ -16,8 +17,10 @@ import path from "path";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 //middleware
 app.use(cookieParser());
+app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
