709d447744a4637463e4c35ec5e467cd00520397
utku <utkbktss5@gmail.com>
2024-10-27 01:18:20 +0300

    host vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
