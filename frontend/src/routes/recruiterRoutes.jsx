df138d0703af28a8a6c25c1d52b2da910c76af7d
utku <utkbktss5@gmail.com>
2024-10-27 00:23:50 +0300

    Lazy-loading

--- frontend/src/routes/recruiterRoutes.jsx
+++ frontend/src/routes/recruiterRoutes.jsx
@@ -1,37 +1,108 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { Suspense } from "react";
 import NotFound from "@/components/notFound/notFound";
-import RecruiterLayout from "@/layouts/recruiterLayout";
+const RecruiterLayout = React.lazy(() => import("@/layouts/recruiterLayout"));
+const RecruiterDashboard = React.lazy(() =>
+  import("@/pages/recruiter/dashboard/recruiterDashboard")
+);
+const RecruiterCreate = React.lazy(() =>
+  import("@/pages/recruiter/create/recruiterCreate")
+);
+const CompanyCreate = React.lazy(() =>
+  import("@/pages/recruiter/companyCreate/companyCreate")
+);
+const CompanyDetails = React.lazy(() =>
+  import("@/pages/recruiter/companyCreate/partials/companyDetails")
+);
+const MyAds = React.lazy(() => import("@/pages/recruiter/myAds/myAds"));
 import { RecruiterLoaders } from "@/loaders/recruiterLoaders";
-import CompanyCreate from "@/pages/recruiter/companyCreate/companyCreate";
-import CompanyDetails from "@/pages/recruiter/companyCreate/partials/companyDetails";
-import RecruiterCreate from "@/pages/recruiter/create/recruiterCreate";
-import RecruiterDashboard from "@/pages/recruiter/dashboard/recruiterDashboard";
-import MyAds from "../pages/recruiter/myAds/myAds";
+import Loading from "@/components/loading/loading";
 
 export const RecruiterRoutes = {
   path: "/recruiter",
-  element: <RecruiterLayout />,
+  element: (
+    <Suspense
+      fallback={
+        <div>
+          <Loading />
+        </div>
+      }
+    >
+      <RecruiterLayout />
+    </Suspense>
+  ),
   errorElement: <NotFound />,
   loader: () => RecruiterLoaders("işveren"),
   children: [
     {
       path: "dashboard",
-      element: <RecruiterDashboard />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <RecruiterDashboard />
+        </Suspense>
+      ),
     },
     {
       path: "create",
-      element: <RecruiterCreate />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <RecruiterCreate />
+        </Suspense>
+      ),
     },
     {
       path: "companies/create",
-      element: <CompanyCreate />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <CompanyCreate />
+        </Suspense>
+      ),
     },
     {
       path: "companies/companyDetails",
-      element: <CompanyDetails />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <CompanyDetails />
+        </Suspense>
+      ),
     },
     {
       path: "applicant",
-      element: <MyAds />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <MyAds />
+        </Suspense>
+      ),
     },
   ],
 };
--- frontend/src/routes/userRoutes.jsx
+++ frontend/src/routes/userRoutes.jsx
@@ -1,31 +1,90 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { Suspense } from "react";
 import NotFound from "@/components/notFound/notFound";
-import UserLayout from "@/layouts/userLayout";
+const UserLayout = React.lazy(() => import("@/layouts/userLayout"));
+const ProfileCreate = React.lazy(() =>
+  import("@/pages/profile/create/createProfile")
+);
+const ProfileDashboard = React.lazy(() =>
+  import("@/pages/profile/dashboard/profileDashboard")
+);
+const MyProjects = React.lazy(() =>
+  import("@/pages/profile/myProjects/myProjects")
+);
 import { UserLoader } from "@/loaders/userLoaders";
-import ProfileCreate from "@/pages/profile/create/createProfile";
-import ProfileDashboard from "@/pages/profile/dashboard/profileDashboard";
-import MyProjects from "@/pages/profile/myProjects/myProjects";
+import Loading from "@/components/loading/loading";
 
 export const UserRoutes = {
   path: "/profile",
-  element: <UserLayout />,
+  element: (
+    <Suspense
+      fallback={
+        <div>
+          <Loading />
+        </div>
+      }
+    >
+      <UserLayout />
+    </Suspense>
+  ),
   errorElement: <NotFound />,
   loader: () => UserLoader(),
   children: [
     {
       path: "",
-      element: <ProfileDashboard />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <ProfileDashboard />
+        </Suspense>
+      ),
     },
     {
       path: "create",
-      element: <ProfileCreate />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <ProfileCreate />
+        </Suspense>
+      ),
     },
     {
       path: "projects",
-      element: <MyProjects />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <MyProjects />
+        </Suspense>
+      ),
     },
     {
       path: "update/:id",
-      element: <ProfileCreate />,
+      element: (
+        <Suspense
+          fallback={
+            <div>
+              <Loading />
+            </div>
+          }
+        >
+          <ProfileCreate />
+        </Suspense>
+      ),
     },
   ],
 };
