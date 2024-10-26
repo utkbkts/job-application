bfe023dcd82d343f766e3b9daed673940cc6293d
utku <utkbktss5@gmail.com>
2024-10-27 01:09:57 +0300

    index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,6 @@ app.use(
 );
 const __dirname = path.resolve();
 //middleware
-app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
