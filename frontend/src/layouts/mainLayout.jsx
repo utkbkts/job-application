fd30789a3b953ffba97061e172594d6a8dddb4be
utku <utkbktss5@gmail.com>
2024-10-27 17:20:54 +0300

    data-error-fixed

--- frontend/src/layouts/mainLayout.jsx
+++ frontend/src/layouts/mainLayout.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 import Header from "@/components/header/header";
 import { Outlet } from "react-router-dom";
+import { useGetUserQuery } from "../redux/api/userApi";
 
 const MainLayout = () => {
+  const { data } = useGetUserQuery();
   return (
     <div>
       <Header />
