5d2973fa06fa487ee6ffc601aff10e0265f4e9a3
utku <utkbktss5@gmail.com>
2024-10-27 19:57:55 +0300

    reviews

--- frontend/src/pages/jobs/detail/allReviews.jsx
+++ frontend/src/pages/jobs/detail/allReviews.jsx
@@ -44,7 +44,7 @@ const AllReviews = ({ data }) => {
                 <p>{item?.comment}</p>
               </div>
             </div>
-            {user?._id === data?.job?.user?._id && (
+            {user?._id === item?.user?._id && (
               <div>
                 <Button
                   onClick={() => handleClick(item?._id)}
